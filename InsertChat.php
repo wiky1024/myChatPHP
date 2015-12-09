8da31ea199cc8bc0211b69767a509f501d0676e9
wiky1024 <wiky1024@gmail.com>
2015-12-09 21:46:47 +0800

    brand new

---
+++ InsertChat.php
@@ -0,0 +1,10 @@
+﻿<?php
+require 'ConnMyChatMariaDB.php';
+$sql="INSERT INTO T_MyChat (userName, msgTime, msgText)VALUES('$_POST[userName]',now(),'$_POST[msgText]')";
+if (!mysql_query($sql,$conn))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "success";
+mysql_close($con)
+?>
